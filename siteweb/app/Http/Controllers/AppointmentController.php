bed22ed8dcbd413d0c19be90f6dc794815c6a3b3
yasserrida <ridayassir.yr@gmail.com>
2023-04-25 21:39:32 +0100

    add update delete

--- siteweb/app/Http/Controllers/AppointmentController.php
+++ siteweb/app/Http/Controllers/AppointmentController.php
@@ -47,4 +47,33 @@ class AppointmentController extends Controller
         // Rediriger vers la liste des rendez-vous avec un message de succès
         return redirect()->route('appointments.index')->with('success', 'Rendez-vous réservé avec succès !');
     }
+
+    public function update(Request $request, string $id)
+    {
+        // Valider les données du formulaire de réservation
+        $request->validate([
+            'name' => 'required',
+            'date' => 'required|date',
+            // Ajouter d'autres règles de validation selon vos besoins
+        ]);
+
+        // Créer un nouveau rendez-vous
+
+        Appointment::where('id', $id)->update([
+            'name' => $request->get('name'),
+            'date' => $request->get('date'),
+            // Ajouter d'autres champs à remplir selon votre modèle de données
+        ]);
+
+        // Rediriger vers la liste des rendez-vous avec un message de succès
+        return redirect()->route('appointments.index')->with('success', 'Rendez-vous modier avec succès !');
+    }
+
+    public function delete(string $id)
+    {
+        Appointment::where('id', $id)->delete();
+
+        // Rediriger vers la liste des rendez-vous avec un message de succès
+        return redirect()->route('appointments.index')->with('success', 'Rendez-vous supprimer avec succès !');
+    }
 }
