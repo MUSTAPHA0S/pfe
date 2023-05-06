a96d898224c26ff615427d7ec0e62a1a23a25afd
anas.elbaz <elbazanas21@gmail.com>
2023-05-06 02:07:35 +0100

    add permissions last

--- siteweb/routes/web.php
+++ siteweb/routes/web.php
@@ -21,9 +21,7 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::middleware(['auth'])->group(function () {
-
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 });
 
 Route::middleware(['auth','role:user'])->group(function () {
